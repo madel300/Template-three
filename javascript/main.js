6f98f8fa0734272b6be5c103be1e8aaa528e125c
unknown <mohamedadel3001@yahoo.com>
2023-02-20 21:09:37 +0200

    add some change

--- javascript/main.js
+++ javascript/main.js
@@ -11,13 +11,31 @@ document.body.childNodes.forEach(element => {
 });
 
 let ourSkills = document.querySelector(".our-skills");
-let spanBar = document.querySelectorAll(".our-skills .container .skills .skill .bar span")
+let spanBar = document.querySelectorAll(".our-skills .container .skills .skill .bar span");
 
+let stats = document.querySelector(".stats");
+let countNumber = document.querySelectorAll(".stats .container .cards .box .number");
+let start = false;
 window.onscroll = function () {
     if (window.scrollY >= ourSkills.offsetTop - 100) {
         spanBar.forEach(function (span) {
             span.style.width = span.dataset.value;
         })
+    }
+
+    if (window.scrollY >= stats.offsetTop) {
+        if (!start) {
+            countNumber.forEach((num) => {
+                let value = num.dataset.value;
+                let count = setInterval(function () {
+                    num.textContent++;
+                    if (num.textContent === value) {
+                        clearInterval(count);
+                    }
+                }, 2000 / value)
+            });
+            start = true;
+        }
     }
 }
 
@@ -42,5 +60,15 @@ let counter = setInterval(function () {
     document.querySelector(".seconds").innerHTML = (seconds < 10) ? `0${seconds}` : seconds;
 }, 1000);
 
+
+// Show & Hidden Placholder
+let emailInput = document.querySelector("[type = email]");
+let attribute = emailInput.getAttribute("placeholder");
+emailInput.addEventListener("focus",function () {
+    emailInput.setAttribute("placeholder", "");
+    emailInput.onblur = function() {
+        emailInput.setAttribute("placeholder", attribute);
+    }
+});
 
 
